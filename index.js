54cd8e0b65c6f5cffae024027f9b1d3f08dd73c6
Joe Bailey-Roberts <joe.br@humanmade.com>
2022-10-07 18:05:28 +0100

    Remove compiled file

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = require("fs");
-const ciphertext = (0, fs_1.readFileSync)('./ciphertext.txt', 'utf-8');
-console.log(ciphertext);
