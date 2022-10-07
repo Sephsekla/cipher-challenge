d861e37261b895a3fcff4efcd848a6d4b674cacb
Joe Bailey-Roberts <joe.br@humanmade.com>
2022-10-07 18:28:52 +0100

    get indices from text

--- index.ts
+++ index.ts
@@ -3,5 +3,32 @@ import prompt from "prompt";
 
 
 const ciphertext : string = readFileSync('./ciphertext.txt', 'utf-8');
+const charCodeOffset: number = 97; // Offset for character codes relative to alphabet position.
+const alphabetLength: number = 26; // Length of the alphabet.
 
-console.log(ciphertext);
+const ciphertextIndex: number[] = [ ...ciphertext ].map (
+	character => getLetterPosition( character )
+);
+
+function getLetterPosition( character: string ) {
+	const characterCode: number = character.charCodeAt( 0 );
+	return characterCode - charCodeOffset;
+}
+
+prompt.start();
+
+prompt.get( [
+	'encodedLetter',
+	'guessDecodedLetter',
+
+], 
+( err: Error | null, result: { encodedLetter: string, guessDecodedLetter: string } ): void => {
+
+	const { encodedLetter, guessDecodedLetter } = result;
+
+	const offset: number = getLetterPosition( guessDecodedLetter ) - getLetterPosition( encodedLetter );
+
+	console.log( ciphertextIndex );
+	
+	
+} );
